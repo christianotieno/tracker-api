4449d8e55ec1b103804cad58a749a6eff77c819c
christianotieno <oduorchristian@gmail.com>
2020-08-15 02:55:19 +0300

    add test suites for models

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
     delete '/logout', to: 'sessions#destroy'
     get '/logged_in', to: 'sessions#method_logged_in?'
     resources :users,  only: %i[create show index] do
-      resources :illnesses do
-        resources :trackings
+      resources :schedules do
+        resources :tasks
       end
     end
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 # require database cleaner at the top level
-require 'database_cleaner'
+# require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -40,9 +40,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   # config.include RequestSpecHelper, type: :request
-
-  # config.include RequestSpecHelper
-  # config.include ControllerSpecHelper
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
