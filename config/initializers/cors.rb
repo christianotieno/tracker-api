e0cec2e996aa54bd87b51e33ccdf568fa3b7ea16
christianotieno <oduorchristian@gmail.com>
2020-08-25 03:59:37 +0300

    add live link to cors configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://127.0.0.1:3000'
+    origins 'https://react-shedule-tracker-app.netlify.app'
 
     resource '*',
       headers: :any, methods: [:get, :post, :put, :patch, :delete], credentials: true
