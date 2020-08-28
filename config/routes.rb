4426cf109897267553f26fe0e947144ea0ded589
christianotieno <oduorchristian@gmail.com>
2020-08-28 19:39:49 +0300

    remove static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    root 'static_page#home'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
     get '/logged_in', to: 'sessions#method_logged_in?'
